dcfcfd6259681b6a5c735fc3438efb0e2ade950a
indianaeronaut <krishnakant.kaushal@gmail.com>
2023-02-15 14:06:40 +0530

    version v6. Sharing methods using 'module'.

---
+++ v6_buying_backpack/features/step_definitions/buying_backpack_steps.rb
@@ -0,0 +1,17 @@
+require_relative "shopping_helper_page"
+
+include ShoppingHelper
+
+Given('I am logged in saucedemo shopping website') do
+  login
+end
+
+When('I provide my shipping info') do
+  add_backpack_to_cart
+  navigate_to_chechout_information_page
+  provide_checkout_information_and_navigate_to_checkout_overview_page
+end
+
+Then('I should be eble to place an order for backpack successfully') do
+  complete_checkout_successfully
+end
