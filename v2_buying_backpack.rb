69c468fbab659a9c7eeea33650e3862b14dd6296
indianaeronaut <krishnakant.kaushal@gmail.com>
2023-02-15 12:13:28 +0530

    version v2. DRY code.

--- v2_buying_backpack.rb
+++ v2_buying_backpack.rb
@@ -4,61 +4,55 @@ require 'watir'
 # Open chrome browser
 browser = Watir::Browser.new :chrome
 
-# Navigate to saucedemo.com
-browser.goto "https://saucedemo.com"
+# define method for logging in to saucedemo.com
+def method1(browser)
+    browser.goto "https://saucedemo.com"
+    browser.text_field(id: 'user-name').set('standard_user')
+    browser.text_field(id: 'password').set('secret_sauce')
+    browser.button(id: 'login-button').click
+end
 
-# Login to saucedemo.com
-browser.text_field(id: 'user-name').set('standard_user')
-browser.text_field(id: 'password').set('secret_sauce')
-browser.button(id: 'login-button').click
+def method2(browser)
+    browser.button(id: 'add-to-cart-sauce-labs-backpack').click
+end
 
-# Add backpack to cart:
-browser.button(id: 'add-to-cart-sauce-labs-backpack').click
+def method3(browser)
+    browser.a(class: 'shopping_cart_link').click
+    browser.button(id: 'checkout').click
+end
 
-# Navigate to cart page:
-browser.a(class: 'shopping_cart_link').click
+def method4(browser)
+    browser.text_field(id: 'first-name').set('turiyam')
+    browser.text_field(id: 'last-name').set('krishna')
+    browser.text_field(id: 'postal-code').set('78759')
+    browser.button(id: 'continue').click
+end
 
-# Navigate to chechout information page
-browser.button(id: 'checkout').click
+def method5(browser)
+    browser.button(id: 'finish').click
+    puts "Is the cbackpack order placed?: #{browser.div(id: 'checkout_complete_container').child.text == 'THANK YOU FOR YOUR ORDER'}"   
+end
 
-# Provide checkout information and navigate to checkout overview page
-browser.text_field(id: 'first-name').set('turiyam')
-browser.text_field(id: 'last-name').set('krishna')
-browser.text_field(id: 'postal-code').set('78759')
-browser.button(id: 'continue').click
+
+method1(browser) # Login to saucedemo.com
+method2(browser) # Add backpack to cart:
+method3(browser) # Navigate to chechout information page
+method4(browser) # Provide checkout information and navigate to checkout overview page
 sleep 1
-
-# Complete checkout:
-browser.button(id: 'finish').click
-puts "Is the cbackpack order placed?: #{browser.div(id: 'checkout_complete_container').child.text == 'THANK YOU FOR YOUR ORDER'}"
+method5(browser) # Complete checkout successfully:
 sleep 1
 
 # Lets say, you want to buy another backpack. For that you repeat same steps:
-# Navigate to saucedemo.com
-browser.goto "https://saucedemo.com"
-
-# Login to saucedemo.com
-browser.text_field(id: 'user-name').set('standard_user')
-browser.text_field(id: 'password').set('secret_sauce')
-browser.button(id: 'login-button').click
-
-# Add backpack to cart:
-browser.button(id: 'add-to-cart-sauce-labs-backpack').click
-
-# Navigate to cart page:
-browser.a(class: 'shopping_cart_link').click
-
-# Navigate to chechout information page
-browser.button(id: 'checkout').click
-
-# Provide checkout information and navigate to checkout overview page
-browser.text_field(id: 'first-name').set('turiyam')
-browser.text_field(id: 'last-name').set('krishna')
-browser.text_field(id: 'postal-code').set('78759')
-browser.button(id: 'continue').click
+method1(browser)
+method2(browser)
+method3(browser)
+method4(browser)
+sleep 1
+method5(browser)
 sleep 1
 
-# Complete checkout:
-browser.button(id: 'finish').click
-puts "Is the cbackpack order placed?: #{browser.div(id: 'checkout_complete_container').child.text == 'THANK YOU FOR YOUR ORDER'}"
-sleep 1
+=begin
+Note:
+1. We saw in last version (v1_buying_backpack.rb), we had to write same lines ofcode again to buy another backpack.
+   Here by creating methods we have made the code a bit of DRY (Don't Repeat Yourself.)
+=end
